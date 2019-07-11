1d311dbc62d2eb9475bed8f06b56f3daff7cb021
Niputi <7137178+Niputi@users.noreply.github.com>
2019-07-11 20:59:21 +0200

    .env definition

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        API_PORT: string,
+        DATABASE_PORT: string,
+        DATABAE_PASSWORD: string,
+        DATABAE_USER: string,
+        DATABAE_DATABAE: string
+    }
+}
