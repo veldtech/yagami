23c385a64dd6b25c68043afa90fb5252ad3fa5cc
Niputi <7137178+Niputi@users.noreply.github.com>
2019-07-20 22:52:16 +0200

    added missing key

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
         DATABASE_PORT: string,
         DATABASE_PASSWORD: string,
         DATABASE_USER: string,
+        DATABASE_HOST: string,
         DATABASE: string
     }
 }
--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -8,7 +8,7 @@ import axios from "axios";
 
 const pool = new Pool({
     user: process.env.DATABASE_USER,
-    host: process.env.DATABAE_HOST,
+    host: process.env.DATABASE_HOST,
     database: process.env.DATABASE,
     password: process.env.DATABASE_PASSWORD,
     port: Number(process.env.DATABASE_PORT)
