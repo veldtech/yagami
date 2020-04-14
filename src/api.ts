24c2f41d50ce4859931e6bc0b745266d06d8f4f7
Veld <veldsinkie.mv@gmail.com>
2020-04-14 03:47:00 +0200

    Add temporary migration endpoints

--- src/api.ts
+++ src/api.ts
@@ -10,10 +10,17 @@ import * as userRouter from "./routes/users.router";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// app.get("/api/custom", userRouter.custom)
+// TODO: remove once migration has completed.
+app.get("/api/ship", userRouter.ship);
+app.get("/api/user", userRouter.user);
+app.get("/api/box", imageRouter.box);
+app.get("/api/yugioh", imageRouter.yugioh);
+app.get("/api/disability", imageRouter.disability);
+app.get("/api/tohru", imageRouter.tohru);
+app.get("/api/yagami", imageRouter.yagami);
+
 app.get("/ship", userRouter.ship);
 app.get("/user", userRouter.user);
-
 app.get("/box", imageRouter.box);
 app.get("/yugioh", imageRouter.yugioh);
 app.get("/disability", imageRouter.disability);
