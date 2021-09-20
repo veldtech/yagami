dd3c2ae4bf33e598d24de137a50daccbef7c57c6
Jonathan <36110242+DrFineSir@users.noreply.github.com>
2021-09-19 20:47:24 -0400

    Update users.router.ts

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -43,8 +43,8 @@ function CalculateLevel(exp: number) {
 /**
  * Calculates Miki experience from level
  */
-function CalculateExp(level: number) {
-  var output = level * level * 20
+function CalculateExp(exp: number) {
+  var output = exp * exp * 10;
   return output;
 }
 
@@ -154,7 +154,7 @@ export const user = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   var level = CalculateLevel(user.experience);
-  var expNextLevel = CalculateExp(level + 1);
+  var expNextLevel = CalculateExp(user.experience);
 
   try {
     var background = await axios.get(url, {
